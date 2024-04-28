ed36ef21f748bc9b8c9ec775ca004c6ea87b3a05
Mochammad Rizky Septian <rizkyseptian401@gmail.com>
2024-04-28 23:08:42 +0700

    Menambahkan API mahasiswa

---
+++ tpmodul10_1302220121/Controllers/MahasiswaController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tpmodul10_1302220121.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : Controller
+    {
+        private static List<Mahasiswa> dataMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa {Nama = "Mochammad Rizky Septian", Nim = "1302220121"},
+            new Mahasiswa {Nama = "Muhammad Rafif AryaSatya Purnomo ", Nim = "1302220003"},
+            new Mahasiswa {Nama = "Muhammad Ghaziveda Belvanaufal", Nim = "1302220011"},
+            new Mahasiswa {Nama = "Daffa Adrianto Effendi", Nim = "1302220151"},
+            new Mahasiswa {Nama = "Muhammad Ghiyats Fatiha", Nim = "1302220109"},
+            new Mahasiswa {Nama = "Reihan Ramdhana Anwari ", Nim = "1302220048"},
+            new Mahasiswa {Nama = "Muhammad Zidan Siva Fajar ", Nim = "1302223039"}
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> GetMahasiswa()
+        {
+            return dataMahasiswa;
+        }
+
+        [HttpGet("{id}")]
+        public Mahasiswa GetMahasiswa(int id)
+        {
+            return dataMahasiswa[id];
+        }
+
+        [HttpPost]
+        public void PostMahasiswa(Mahasiswa mahasiswa)
+        {
+            dataMahasiswa.Add(mahasiswa);
+        }
+
+        [HttpPut("{id}")]
+        public void PutMahasiswa(int id, Mahasiswa mahasiswa)
+        {
+            dataMahasiswa[id] = mahasiswa;
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteMahasiswa(int id)
+        {
+            dataMahasiswa.RemoveAt(id);
+        }
+    }
+
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+    }
+}
---
+++ tpmodul10_1302220121/Mahasiswa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace tpmodul10_1302220121
+{
+    public class Mahasiswa
+    {
+        public string Nama {  get; set; }
+        public string Nim {  get; set; }
+
+        public Mahasiswa(string Nama, string Nim)
+        {
+            this.Nama = Nama;
+            this.Nim = Nim;
+        }
+    }
+}
